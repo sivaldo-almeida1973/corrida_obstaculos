1fd800b8e05bce78cecc98483f30625a838b2a9a
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-11-25 22:00:21 -0300

    Update corrida_obstaculos.py

--- corrida_obstaculos.py
+++ corrida_obstaculos.py
@@ -25,4 +25,50 @@ imagemChao = pygame.transform.scale2x( pygame.image.load(os.path.join('imagens',
 imagemFundo = pygame.image.load(os.path.join('imagens','bg.png'))
 imagemFundoPontos = pygame.image.load(os.path.join('imagens','fundoPontos.png'))
 
+#criar classes-=------------------
+class PapaiNoel:
+  imagens = imagemPersonagem #lista imagemPersonagem
+  noChao = False  #true ta no cao e false ta pulando
+
+  def __init__(self, posx, posy):
+     self.posx = posx
+     self.posy = posy
+     self.velocidadePersonagem = 0  #controla a velocidade do papai noel
+     self.tempoJogo = 0
+     self.imagem = self.imagens(0)
+
+  def pular(self):
+     if self.posy >= 240: #pular
+      self.velocidadePersonagem = -11 #para baixo ->positivo, para cima -> negativo
+      self.tempoJogo = 0
+
+  
+  def movimento(self):
+    self.tempoJogo += 1
+    aceleracao = 3
+    deslocamentoPersonagem = self.velocidadePersonagem * self.tempoJogo \
+                                                + (self.tempoJogo**2)/2
+    
+    if deslocamentoPersonagem > 11:  #vai pra baixo(quanto maior o numero, maior a velocidade da queda)
+      deslocamentoPersonagem = 11
+    elif deslocamentoPersonagem < 0:
+      deslocamentoPersonagem -= 10 #pra cima
+
+    self.posy +=  deslocamentoPersonagem
+
+    if self.imagem.index(self.imagem) == 0:# papai noel deitado
+      
+
+
+
+
+
+  
+
+
+
+     
+   
+
+
 
