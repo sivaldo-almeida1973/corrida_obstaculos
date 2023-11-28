2266a21801d4ab7e379b20cf77ed2b6b3eb44232
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-11-27 21:20:19 -0300

    finalizando projeto

--- corrida_obstaculos.py
+++ corrida_obstaculos.py
@@ -21,7 +21,7 @@ imagemObstaculos = [
 ]
 
 #pygame.transform.scale2x(dobra o tamanho da imagem)
-imagemChao = pygame.transform.scale2x( pygame.image.load(os.path.join('imagens','chao.))png')))
+imagemChao = pygame.transform.scale2x( pygame.image.load(os.path.join('imagens','chao.png')))
 imagemFundo = pygame.image.load(os.path.join('imagens','bg.png'))
 imagemFundoPontos = pygame.image.load(os.path.join('imagens','fundoPontos.png'))
 
@@ -35,7 +35,7 @@ class PapaiNoel:
      self.posy = posy
      self.velocidadePersonagem = 0  #controla a velocidade do papai noel
      self.tempoJogo = 0
-     self.imagem = self.imagens(0)
+     self.imagem = self.imagens[0]
 
   def pular(self):
      if self.posy >= 240: #pular
@@ -46,7 +46,7 @@ class PapaiNoel:
     self.tempoJogo += 1
     aceleracao = 3
     deslocamentoPersonagem = self.velocidadePersonagem * self.tempoJogo \
-                                                + (self.tempoJogo**2)/2
+                                                +(aceleracao*(self.tempoJogo**2))/2
     
     if deslocamentoPersonagem > 11: #pra baixo( maior o num, maior a veloc da queda)
       deslocamentoPersonagem = 11
@@ -126,9 +126,11 @@ class Obstaculo:
     elif self.proxObstaculo == 2:
       janela.blit(self.obstaculoCima, (self.posx, self.posicaoObstaculoAlto))#desenhar janela
       
-  def testar_coliao(self):
+  def testar_colisao(self, PapaiNoel):
+
     papaiNoelSuperficie = PapaiNoel.superficiePersonagem()
     ObstaculoBaixoSuperficie = pygame.mask.from_surface(self.obstaculoBaixo)
+    
     ObstaculoCimaSuperficie = pygame.mask.from_surface(self.obstaculoCima)
 
     testeColisao = False
@@ -193,6 +195,98 @@ def main():
   pontos = 0
   relogio = pygame.time.Clock()
   vivo = True
+
+  while vivo:
+    relogio.tick(30)
+
+    for evento in pygame.event.get():
+      if evento.type == pygame.QUIT:#x para encerrar
+        vivo = False
+        pygame.quit()
+        quit()
+      if evento.type == pygame.KEYDOWN:
+        if evento.key == pygame.K_SPACE:#Pular tecla space
+          noel.imagem = noel.imagens[0]
+          noel.pular()
+      if evento.type == pygame.KEYDOWN: #Abaixar
+        if evento.key == pygame.K_DOWN: #setinha pra baixo
+          noel.imagem = noel.imagens[1]
+      if evento.type == pygame.KEYUP: #Levantar
+        if evento.key == pygame.K_DOWN:
+          noel.imagem = noel.imagens[0]
+
+
+    indice_obs = 0
+    if vivo:
+      if len(obstaculos) > 1 and noel.posx > (obstaculos[0].posx + obstaculos[0].obstaculoBaixo.get_width()):
+        indice_obs = 1
+    
+    noel.movimento()
+    chao.movimento()
+
+    novoObstaculo = False
+    removerObstaculos = []
+    for obstaculo in obstaculos:
+      if obstaculo.testar_colisao(noel):
+        vivo = False
+        time.sleep(1)
+        print(f'\n********** Pontuação: {pontos} **********')
+      if not obstaculo.passouObstaculo and noel.posx > obstaculo.posx:               
+        obstaculo.passouObstaculo = True 
+        novoObstaculo = True
+      obstaculo.movimento()
+      if obstaculo.posx + obstaculo.obstaculoBaixo.get_width() < 0:
+        removerObstaculos.append(obstaculo)
+    
+    if novoObstaculo:
+      pontos += 1
+      distProxObstaculo = 600 + 30 * pontos
+      if distProxObstaculo < 960:
+        obstaculos.append(Obstaculo(distProxObstaculo))
+      else:
+        obstaculos.append(Obstaculo(960))
+    for obstaculo in removerObstaculos:
+      obstaculos.remove(obstaculo)
+
+    for obstaculo in obstaculos:
+      if obstaculo.velocidadeObstaculo < 30:
+        obstaculo.velocidadeObstaculo = 10 + round(pontos/3)
+      else:
+        obstaculo.velocidadeObstaculo = 30
+      
+      if chao.velocidadeChao < 30:
+        chao.velocidadeChao = 10 + round(pontos/3)
+      else:
+        chao.velocidadeChao = 30
+
+    desenhar_janela(janela, noel, obstaculos, chao, pontos)
+
+if __name__ == '__main__':
+  main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   
 
